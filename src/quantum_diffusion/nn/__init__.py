f26d7f6341546e80f1c43ff324cc7b61d91d0c0f
Jan Smółka <jp.smolka@gmail.com>
2025-10-18 19:42:24 +0200

    Obtain models and datasets more reliably

--- src/quantum_diffusion/nn/__init__.py
+++ src/quantum_diffusion/nn/__init__.py
@@ -3,10 +3,111 @@ Module contains all neural networks used in the project.
 Some are not used anymore, but are kept for reference and marked as deprecated.
 """
 
-from .qconv import QConv2d
-from .qdense import *
-from .unet import UnetDirected, UNetUndirected
+from typing import Any, Literal
+
+from torchvision.ops.poolers import torch
+
+from .conv import DeepConvDirectedMulti, DeepConvDirectedSingle, DeepConvUndirected
 from .dense import DenseDirected, DenseUndirected
-from .utils import *
-from .unet_simple import UNetUndirectedS, UnetDirectedS
-from .conv import *
+from .qconv import QConv2d
+from .qdense import (
+    QDense2Undirected,
+    QDense4StatesAncilla,
+    QDense4StatesUndirected,
+    QDenseDirected,
+    QDenseDirectedReupload,
+    QDenseUndirected,
+)
+from .unet import UnetDirected, UNetUndirected
+from .unet_simple import UnetDirectedS, UNetUndirectedS
+from .utils import (
+    autocrop,
+    autopad,
+    circuit_to_qasm,
+    get_label_embedding,
+    repeat_qasm,
+    sample_from_qiskit,
+)
+
+Model = Literal[
+    "DeepConvDirectedMulti",
+    "DeepConvDirectedSingle",
+    "DeepConvUndirected",
+    "DenseDirected",
+    "DenseUndirected",
+    "QConv2d",
+    "QDense2Undirected",
+    "QDense4StatesAncilla",
+    "QDense4StatesUndirected",
+    "QDenseDirected",
+    "QDenseDirectedReupload",
+    "QDenseUndirected",
+    "UnetDirected",
+    "UNetUndirected",
+    "UnetDirectedS",
+    "UNetUndirectedS",
+]
+
+
+def get_by_name(model: Model, parameters: list[Any]) -> torch.nn.Module:
+    match model:
+        case "DeepConvDirectedMulti":
+            return DeepConvDirectedMulti(*parameters)
+        case "DeepConvDirectedSingle":
+            return DeepConvDirectedSingle(*parameters)
+        case "DeepConvUndirected":
+            return DeepConvUndirected(*parameters)
+        case "DenseDirected":
+            return DenseDirected(*parameters)
+        case "DenseUndirected":
+            return DenseUndirected(*parameters)
+        case "QConv2d":
+            return QConv2d(*parameters)
+        case "QDense2Undirected":
+            return QDense2Undirected(*parameters)
+        case "QDense4StatesAncilla":
+            return QDense4StatesAncilla(*parameters)
+        case "QDense4StatesUndirected":
+            return QDense4StatesUndirected(*parameters)
+        case "QDenseDirected":
+            return QDenseDirected(*parameters)
+        case "QDenseDirectedReupload":
+            return QDenseDirectedReupload(*parameters)
+        case "QDenseUndirected":
+            return QDenseUndirected(*parameters)
+        case "UnetDirected":
+            return UnetDirected(*parameters)
+        case "UNetUndirected":
+            return UNetUndirected(*parameters)
+        case "UnetDirectedS":
+            return UnetDirectedS(*parameters)
+        case "UNetUndirectedS":
+            return UNetUndirectedS(*parameters)
+
+
+__all__ = [
+    "Model",
+    "get_by_name",
+    "DeepConvDirectedMulti",
+    "DeepConvDirectedSingle",
+    "DeepConvUndirected",
+    "DenseDirected",
+    "DenseUndirected",
+    "QConv2d",
+    "QDense2Undirected",
+    "QDense4StatesAncilla",
+    "QDense4StatesUndirected",
+    "QDenseDirected",
+    "QDenseDirectedReupload",
+    "QDenseUndirected",
+    "UnetDirected",
+    "UNetUndirected",
+    "UnetDirectedS",
+    "UNetUndirectedS",
+    "autocrop",
+    "autopad",
+    "circuit_to_qasm",
+    "get_label_embedding",
+    "repeat_qasm",
+    "sample_from_qiskit",
+]
