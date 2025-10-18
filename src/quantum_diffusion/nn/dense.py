f3095ffff0ba745c263e9dedf55e6552ee557a71
Jan Smółka <jp.smolka@gmail.com>
2025-10-18 22:14:07 +0200

    Add a partial typing to `dense`, `conv` and `qconv` modules

--- src/quantum_diffusion/nn/dense.py
+++ src/quantum_diffusion/nn/dense.py
@@ -1,30 +1,38 @@
 import einops
 import torch
+from typing_extensions import override
 
 
 class DenseUndirected(torch.nn.Module):
     """Dense neural network. Undirected"""
 
-    def __init__(self, shapes) -> None:
+    shapes: list[int]
+    net: torch.nn.Sequential
+
+    def __init__(self, shapes: list[int]) -> None:
         super().__init__()
         assert shapes[0] == shapes[-1], "Input and output shapes must be equal"
-        layers = []
+
         self.shapes = shapes
+
+        layers: list[torch.nn.Module] = []
         for i in range(len(shapes) - 1):
             layers.append(
                 torch.nn.Linear(in_features=shapes[i], out_features=shapes[i + 1])
             )
             layers.append(torch.nn.ReLU())
+
         self.net = torch.nn.Sequential(*layers)
 
-    def forward(self, x):
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
         _, _, w, h = x.shape
         x = einops.rearrange(x, "b 1 w h -> b (w h)")
-        x = self.net(x)
+        x = self.net.forward(x)
         x = einops.rearrange(x, "b (w h) -> b 1 w h", w=w, h=h)
         return x
 
-    def __repr__(self):
+    @override
+    def __repr__(self) -> str:
         return f"DenseUndirected({self.net})"
 
     def save_name(self) -> str:
@@ -34,21 +42,28 @@ class DenseUndirected(torch.nn.Module):
 class DenseDirected(torch.nn.Module):
     """Dense neural network with label"""
 
-    def __init__(self, shapes) -> None:
+    shapes: list[int]
+    net: torch.nn.Sequential
+
+    def __init__(self, shapes: list[int]) -> None:
         super().__init__()
         assert shapes[0] == shapes[-1], "Input and output shapes must be equal"
-        layers = []
+
         self.shapes = shapes
+
         shapes = list(shapes)
         shapes[0] += 1  # Add label
+
+        layers: list[torch.nn.Module] = []
         for i in range(len(shapes) - 1):
             layers.append(
                 torch.nn.Linear(in_features=shapes[i], out_features=shapes[i + 1])
             )
             layers.append(torch.nn.ReLU())
+
         self.net = torch.nn.Sequential(*layers)
 
-    def forward(self, x, y):
+    def forward(self, x: torch.Tensor, y: torch.Tensor) -> torch.Tensor:
         _, _, w, h = x.shape
         y = y.unsqueeze(-1)
         x = einops.rearrange(x, "b 1 w h -> b (w h)")
@@ -57,7 +72,8 @@ class DenseDirected(torch.nn.Module):
         x = einops.rearrange(x, "b (w h) -> b 1 w h", w=w, h=h)
         return x
 
-    def __repr__(self):
+    @override
+    def __repr__(self) -> str:
         return f"DenseDirected({self.net})"
 
     def save_name(self) -> str:
